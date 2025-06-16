236dfcc079412a9bc11ba12d11b5fafee92de564
mohit <121346078+mannmohhh@users.noreply.github.com>
2025-06-16 10:59:51 +0530

    Update python.py

--- zigzag conversion problem/python.py
+++ zigzag conversion problem/python.py
@@ -1,1 +1,16 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        if numRows == 1 or numRows >= len(s):
+            return s
+        rows = [''] * numRows
+        i = 0
+        step = 1
+        for c in s:
+            rows[i] += c
+            if i == 0:
+                step = 1
+            elif i == numRows - 1:
+                step = -1
+            i += step
+        return ''.join(rows)
 
